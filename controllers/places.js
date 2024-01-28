e8f069806767ff851de45cdfeae629590fd6cdf1
zephyr <jafaribrownsha@gmail.com>
2024-01-28 00:13:36 -0500

    add more stuff to places, post route and upgraded

--- controllers/places.js
+++ controllers/places.js
@@ -1,25 +1,44 @@
 const router = require("express").Router();
+const places = require("../models/places.js");
 
 // More code here in a moment
+//places new
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
+
+//post
+router.post("/", (req, res) => {
+  console.log(req.body);
+  // Dig into req.body and make sure data is valid
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+
+  res.redirect("/places");
+});
+
+//get id
+// router.get("/:id", (req, res) => {
+//   let id = Number(req.params.id);
+//   if (isNaN(id)) {
+//     res.render("error404");
+//   } else if (!places[id]) {
+//     res.render("error404");
+//   } else {
+//     res.render("places/show", { place: places[id], id });
+//   }
+// });
 // GET /places
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/goku.png",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/gurpartap-sandhu-nuAOmcjs3ZQ-unsplash.jpg",
-    },
-  ];
-
   res.render("places/index", { places });
 });
 
