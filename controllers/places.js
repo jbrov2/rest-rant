825f1d627c11170a77e3298270de0b686523e227
zephyr <jafaribrownsha@gmail.com>
2024-02-06 00:36:55 -0500

    comment time

--- controllers/places.js
+++ controllers/places.js
@@ -129,7 +129,16 @@ router.get("/new", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  res.send("GET /places/:id stub");
+  db.Place.findById(req.params.id)
+    .populate("comments")
+    .then((place) => {
+      console.log(place.comments);
+      res.render("places/show", { place });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
 
 router.put("/:id", (req, res) => {
@@ -145,6 +154,20 @@ router.get("/:id/edit", (req, res) => {
 });
 
 router.post("/:id/rant", (req, res) => {
+  console.log(req.body);
+  req.body.rant = req.body.rant ? true : false;
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      db.Comment.create(req.body).then((comment) => {
+        place.comments.push(comment.id);
+        place.save().then(() => {
+          res.redirect(`/places/${req.params.id}`);
+        });
+      });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
   res.send("GET /places/:id/rant stub");
 });
 
